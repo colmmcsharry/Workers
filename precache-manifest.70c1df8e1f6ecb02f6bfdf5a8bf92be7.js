ba086438f6be3c4bae86ceea25ec7127b3d53c76
Colm McSharry <colmmcsharry@hotmail.com>
2019-11-18 16:41:01 -0800

    Updates

---
+++ precache-manifest.70c1df8e1f6ecb02f6bfdf5a8bf92be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1c6ce88935450d57e8fbad24a76fff",
+    "url": "/Workers/index.html"
+  },
+  {
+    "revision": "6a09f0589529cae0c2d9",
+    "url": "/Workers/static/css/2.a4e54d08.chunk.css"
+  },
+  {
+    "revision": "3184b4ef7043ba89d000",
+    "url": "/Workers/static/css/main.97055fba.chunk.css"
+  },
+  {
+    "revision": "6a09f0589529cae0c2d9",
+    "url": "/Workers/static/js/2.d5c104f9.chunk.js"
+  },
+  {
+    "revision": "3184b4ef7043ba89d000",
+    "url": "/Workers/static/js/main.ab1aa899.chunk.js"
+  },
+  {
+    "revision": "7298b13393d0921269c1",
+    "url": "/Workers/static/js/runtime~main.18040379.js"
+  },
+  {
+    "revision": "30fc6ca6ea99b8712e1f8064258e694a",
+    "url": "/Workers/static/media/networking.30fc6ca6.png"
+  }
+]);
